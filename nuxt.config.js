2e4954bae364308be8b5753dd0842f0c7d2ffc50
denispan <6231990@gmail.com>
2023-09-10 12:52:59 +0200

    add auth

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,43 +1,53 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'quwi-test',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/auth'
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL:  'https://api.quwi.com/v2/'
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: { url: '/auth/login', method: 'post', propertyName: 'token' },
+          logout: { url: '/auth/logout', method: 'post' },
+          user: { url: '/auth/init', method: 'get' }
+        },
+        tokenType: 'Bearer'
+      },
+      redirect: {
+        login: '/login', // пользователь будет перенаправлен на эту страницу, если попытается получить доступ к защищенной странице и он не аутентифицирован
+        logout: '/', // куда перенаправлять после выхода
+        callback: '/login',
+        home: '/' // куда перенаправлять после успешного входа
+      }
+    }
+  },
+
+  build: {}
 }
---
+++ pages/login.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="login-page">
+    <h1>Авторизация</h1>
+    <form @submit.prevent="login">
+      <div>
+        <label for="email">Логин:</label>
+        <input id="email" v-model="email" required>
+      </div>
+      <div>
+        <label for="password">Пароль:</label>
+        <input type="password" id="password" v-model="password" required>
+      </div>
+      <button type="submit">Войти</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        await this.$auth.loginWith('local', {
+          data: {
+            email: this.email,
+            password: this.password
+          }
+        });
+        await this.$router.push('/'); // перенаправляем пользователя на главную страницу после успешной авторизации
+      } catch (error) {
+        console.error('Ошибка авторизации:', error);
+      }
+    }
+  },
+  middleware: ['auth']
+};
+</script>
+
+<style scoped>
+.login-page {
+  max-width: 400px;
+  margin: 50px auto;
+  padding: 20px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+}
+</style>
