5bff84e730a96c1a75c527e1ae3ba3ceba0b782d
denispan <6231990@gmail.com>
2023-09-11 18:01:43 +0200

    project styles

--- components/Project.vue
+++ components/Project.vue
@@ -2,8 +2,16 @@
   <div class="project" @click="handleClick">
     <div class="project__content">
       <img class="project__logo" :src="project.logo_url !== null ? project.logo_url : require('~/assets/images/no-logo.png')" alt="Logo">
-      <h2>{{ project.name }}</h2>
-      <p v-if="project.is_active === 1">Active</p>
+      <h2 class="project__title">{{ project.name }}</h2>
+      <p class="project__status" v-if="project.is_active === 1">Active</p>
+      <div class="project__time">
+        <p>
+          Время в проекте
+        </p>
+        <p>
+          {{ formatTime(project.spent_sec_all_time) }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -25,6 +33,20 @@ export default {
   methods: {
     handleClick() {
       this.$emit('openModal', this.project);
+    },
+    formatTime(seconds) {
+      let days = Math.floor(seconds / (24 * 60 * 60));
+      seconds -= days * (24 * 60 * 60);
+
+      let hours = Math.floor(seconds / (60 * 60));
+      seconds -= hours * (60 * 60);
+
+      let minutes = Math.floor(seconds / 60);
+      seconds -= minutes * 60;
+
+      return days.toString().padStart(2, '0') + ':' +
+        hours.toString().padStart(2, '0') + ':' +
+        minutes.toString().padStart(2, '0');
     }
   },
 }
@@ -49,10 +71,36 @@ export default {
   gap: 3rem;
   grid-template-columns: min-content 2fr 1fr 1fr;
   align-items: center;
+
+  @include vp-767 {
+    grid-template-columns: 1fr;
+    text-align: center;
+    gap: 1rem;
+  }
 }
 
 .project__logo {
   width: 100px;
   height: 100%;
+
+  @include vp-767 {
+    margin: 0 auto;
+  }
+}
+
+.project__status {
+  font-size: 1.5rem;
+  color: green;
+}
+
+.project__time {
+  display: flex;
+  align-items: center;
+  font-weight: 500;
+  gap: 1.5rem;
+
+  @include vp-767 {
+    margin: 0 auto;
+  }
 }
 </style>
